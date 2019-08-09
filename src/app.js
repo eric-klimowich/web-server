9ba59c4383bf4edecaed6f38e3c143fc9d6de53b
eric-klimowich <eklimowich@hotmail.com>
2019-08-09 17:25:03 -0400

    Added additional routes for help, about, and weather

--- src/app.js
+++ src/app.js
@@ -6,6 +6,18 @@ app.get('', (req, res) => {
     res.send('Hello express!')
 })
 
+app.get('/help', (req, res) => {
+    res.send('Help is on the way!')
+})
+
+app.get('/about', (req, res) => {
+    res.send("What it's all about!")
+})
+
+app.get('/weather', (req, res) => {
+    res.send("Better put your booties on because it's cold outside!")
+})
+
 // app.com
 // app.com/help
 // app.com/about
