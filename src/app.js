8755e101d6ed19697ba49ee60573f71a01ae54d8
eric-klimowich <eklimowich@hotmail.com>
2019-08-10 08:39:26 -0400

    Installed handlebars(hbs), moved index.html to index.hbs, deleted public index.html, rendering the handlebars view

--- src/app.js
+++ src/app.js
@@ -5,10 +5,26 @@ const express = require('express')
 // console.log(path.join(__dirname, '../public'))
 
 const app = express()
-
 const publicDirectoryPath = path.join(__dirname, '../public')
 
+app.set('view engine', 'hbs')
 app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+    res.render('index')
+})
+
+app.get('/weather', (req, res) => {
+    res.send({
+        address: 'New York',
+        temperature: 50
+    })
+})
+
+app.listen(3000, () => { // optional argument
+    console.log('Server is up on port 3000.')
+})
+
 
 // app.get('', (req, res) => {
 //     res.send('<h1>Hello express!</h1>')
@@ -21,14 +37,3 @@ app.use(express.static(publicDirectoryPath))
 // app.get('/about', (req, res) => {
 //     res.send("<h1>What it's all about!</h1>")
 // })
-
-app.get('/weather', (req, res) => {
-    res.send({
-        address: 'New York',
-        temperature: 50
-    })
-})
-
-app.listen(3000, () => { // optional argument
-    console.log('Server is up on port 3000.')
-})
