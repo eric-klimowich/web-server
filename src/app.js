6ef1c82c553c39bb2357ab9d7edc7b00b6c6f632
eric-klimowich <eklimowich@hotmail.com>
2019-08-10 09:16:45 -0400

    Set up templates folder to hold views and partials, changed path to views, and added path for partials

--- src/app.js
+++ src/app.js
@@ -5,11 +5,17 @@ const express = require('express')
 // console.log(path.join(__dirname, '../public'))
 
 const app = express()
-const publicDirectoryPath = path.join(__dirname, '../public')
-const viewsPath = path.join(__dirname, '../templates')
 
+// Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+// Setup handlebars engine and views location
 app.set('view engine', 'hbs')
 app.set('views', viewsPath)
+
+// Setup up static director to serve
 app.use(express.static(publicDirectoryPath))
 
 app.get('', (req, res) => {
