cdae0161fb22a2f35ed7c255e69f7c8ac6054590
eric-klimowich <eklimowich@hotmail.com>
2019-08-11 12:10:10 -0400

    Used fetch to log data from endpoint with query string to the console

--- public/js/app.js
+++ public/js/app.js
@@ -9,3 +9,15 @@ fetch('http://puzzle.mead.io/puzzle', ).then((response) => {
 fetch('http://puzzle.mead.io/puzzle', )
     .then(res => res.json())
     .then(data => console.log(data))
+
+
+fetch('http://localhost:3000/weather?address=boston', ).then((response) => {
+    response.json().then((data) => {
+        if (data.error) {
+            console.log(data.error)
+        } else {
+            console.log(data.location)
+            console.log(data.forecast)
+        }
+    })
+})
