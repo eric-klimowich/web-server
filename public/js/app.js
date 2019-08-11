3e5b716237c41e651e0afe2c74c513fade8f090f
eric-klimowich <eklimowich@hotmail.com>
2019-08-11 12:30:29 -0400

    Searched for form, added eventListener, prevented default, and tested functionality

--- public/js/app.js
+++ public/js/app.js
@@ -1,14 +1,14 @@
 console.log('Client side JavaScript file is loaded.')
 
-fetch('http://puzzle.mead.io/puzzle', ).then((response) => {
-        response.json().then((data) => {
-            console.log(data)
-        })
-    })
+// fetch('http://puzzle.mead.io/puzzle', ).then((response) => {
+//         response.json().then((data) => {
+//             console.log(data)
+//         })
+//     })
 
-fetch('http://puzzle.mead.io/puzzle', )
-    .then(res => res.json())
-    .then(data => console.log(data))
+// fetch('http://puzzle.mead.io/puzzle', )
+//     .then(res => res.json())
+//     .then(data => console.log(data))
 
 
 fetch('http://localhost:3000/weather?address=boston', ).then((response) => {
@@ -21,3 +21,12 @@ fetch('http://localhost:3000/weather?address=boston', ).then((response) => {
         }
     })
 })
+
+const weatherForm = document.querySelector('form')
+
+weatherForm.addEventListener('submit', (event) => {
+    event.preventDefault()
+    console.log('clicked')
+})
+
+console.log(weatherForm)
