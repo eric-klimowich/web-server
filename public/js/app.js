4eeb67d2ad72e3f6e149a435d1404a463fda8c89
eric-klimowich <eklimowich@hotmail.com>
2019-08-11 13:03:54 -0400

    Rendered error message when errors occur to home page

--- public/js/app.js
+++ public/js/app.js
@@ -41,7 +41,8 @@ weatherForm.addEventListener('submit', (event) => {
     fetch(`http://localhost:3000/weather?address=${location}`, ).then((response) => {
     response.json().then((data) => {
         if (data.error) {
-            console.log(data.error)
+            messageOne.textContent = data.error
+            // console.log(data.error)
         } else {
             console.log(data.location)
             console.log(data.forecast)
