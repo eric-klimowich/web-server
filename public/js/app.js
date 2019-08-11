a6ce5cdbc44d0af6b0f9de1913a20a96d8b61459
eric-klimowich <eklimowich@hotmail.com>
2019-08-11 12:48:31 -0400

    Used value of location from input to interpolate within the fetch endpoint, providing the correct output

--- public/js/app.js
+++ public/js/app.js
@@ -11,7 +11,28 @@ console.log('Client side JavaScript file is loaded.')
 //     .then(data => console.log(data))
 
 
-fetch('http://localhost:3000/weather?address=boston', ).then((response) => {
+// fetch('http://localhost:3000/weather?address=boston', ).then((response) => {
+//     response.json().then((data) => {
+//         if (data.error) {
+//             console.log(data.error)
+//         } else {
+//             console.log(data.location)
+//             console.log(data.forecast)
+//         }
+//     })
+// })
+
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+
+weatherForm.addEventListener('submit', (event) => {
+    event.preventDefault()
+    // console.log('clicked')
+
+    const location = search.value
+    // console.log(location)
+
+    fetch(`http://localhost:3000/weather?address=${location}`, ).then((response) => {
     response.json().then((data) => {
         if (data.error) {
             console.log(data.error)
@@ -21,15 +42,6 @@ fetch('http://localhost:3000/weather?address=boston', ).then((response) => {
         }
     })
 })
-
-const weatherForm = document.querySelector('form')
-const search = document.querySelector('input')
-
-weatherForm.addEventListener('submit', (event) => {
-    event.preventDefault()
-    console.log('clicked')
-    const location = search.value
-    console.log(location)
 })
 
 console.log(weatherForm)
