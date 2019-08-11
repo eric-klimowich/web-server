8c3930c60c78735963e048ffafc08b49e36d0841
eric-klimowich <eklimowich@hotmail.com>
2019-08-11 11:25:14 -0400

    Practice using fetch to grab data from JSON API

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,11 @@
 console.log('Client side JavaScript file is loaded.')
+
+fetch('http://puzzle.mead.io/puzzle', ).then((response) => {
+        response.json().then((data) => {
+            console.log(data)
+        })
+    })
+
+fetch('http://puzzle.mead.io/puzzle', )
+    .then(res => res.json())
+    .then(data => console.log(data))
